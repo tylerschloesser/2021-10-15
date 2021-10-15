9c0dd2b5d2ef21cb9e79d1e5d1102a1a31ee2201
Tyler Schloesser <tschloes@amazon.com>
2021-10-15 07:01:42 -0700

    fill canvas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 console.log('hi')
+const canvas = document.querySelector('canvas')!
+const context = canvas.getContext('2d')!
+
+const rect = canvas.getBoundingClientRect()
+canvas.height = rect.height
+canvas.width = rect.width
+
+context.fillStyle = 'grey'
+context.fillRect(0, 0, canvas.width, canvas.height)
