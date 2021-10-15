0b91b7b519e017ef114f5e1dff17ca8e78b607ee
Tyler Schloesser <tschloes@amazon.com>
2021-10-15 07:24:11 -0700

    add resize handler

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,27 @@ const rect = canvas.getBoundingClientRect()
 canvas.height = rect.height
 canvas.width = rect.width
 
+window.onresize = () => {
+  const rect = canvas.getBoundingClientRect()
+  canvas.height = rect.height
+  canvas.width = rect.width
+}
+
 function onFrame(timestamp: number) {
   context.fillStyle = 'grey'
   context.fillRect(0, 0, canvas.width, canvas.height)
 
+  const diff = canvas.width - canvas.height
   const size = Math.min(canvas.width, canvas.height)
   const padding = size / 10
 
+  let px = diff > 0 ? padding + diff / 2 : padding
+  let py = diff < 0 ? padding + -diff / 2 : padding
+
   renderGrid({
     context,
-    x: padding,
-    y: padding,
+    x: px,
+    y: py,
     w: padding * 8,
     h: padding * 8,
     rows: 10,
