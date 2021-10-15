ff5128ea566b244bb01af17cb1ae701bf968e3d2
Tyler Schloesser <tschloes@amazon.com>
2021-10-15 07:55:37 -0700

    tick state

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { State } from './game'
+import { State, tick } from './game'
 import { renderGrid } from './render'
 
 const canvas = document.querySelector('canvas')!
@@ -20,7 +20,18 @@ let state: State = {
   pieces: [{ row: 0, col: 4 }],
 }
 
+const TICK_INTERVAL = 1000
+let lastTick: null | number = null
+
 function onFrame(timestamp: number) {
+  if (lastTick === null) {
+    lastTick = timestamp
+  }
+  if (timestamp - lastTick > TICK_INTERVAL) {
+    lastTick = lastTick + TICK_INTERVAL
+    state = tick(state)
+  }
+
   context.fillStyle = 'grey'
   context.fillRect(0, 0, canvas.width, canvas.height)
 
