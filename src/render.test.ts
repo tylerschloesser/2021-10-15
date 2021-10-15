dfa359858abfa368e2a67ac5e3a3afe9728875d9
Tyler Schloesser <tschloes@amazon.com>
2021-10-15 10:11:52 -0700

    test render game over

--- src/render.test.ts
+++ src/render.test.ts
@@ -1,5 +1,5 @@
 import { State } from './game'
-import { renderFloor, renderGrid, renderPieces } from './render'
+import { renderFloor, renderGameOver, renderGrid, renderPieces } from './render'
 
 const DEFAULT_STATE: State = {
   rows: 2,
@@ -10,84 +10,122 @@ const DEFAULT_STATE: State = {
 }
 
 describe('render/renderGrid', () => {
-  it('renders a small grid', () => {})
+  it('renders a small grid', () => {
+    const canvas = <HTMLCanvasElement>{
+      width: 10,
+      height: 10,
+    }
 
-  const canvas = <HTMLCanvasElement>{
-    width: 10,
-    height: 10,
-  }
+    const context = {
+      moveTo: jest.fn(),
+      lineTo: jest.fn(),
+      stroke: jest.fn(),
+    }
 
-  const context = {
-    moveTo: jest.fn(),
-    lineTo: jest.fn(),
-    stroke: jest.fn(),
-  }
+    const state = DEFAULT_STATE
 
-  const state = DEFAULT_STATE
+    renderGrid({ canvas, context: context as any, state })
 
-  renderGrid({ canvas, context: context as any, state })
+    expect(context.moveTo.mock.calls).toEqual([
+      [1, 1],
+      [1, 5],
+      [1, 9],
+      [1, 1],
+      [5, 1],
+      [9, 1],
+    ])
 
-  expect(context.moveTo.mock.calls).toEqual([
-    [1, 1],
-    [1, 5],
-    [1, 9],
-    [1, 1],
-    [5, 1],
-    [9, 1],
-  ])
+    expect(context.lineTo.mock.calls).toEqual([
+      [9, 1],
+      [9, 5],
+      [9, 9],
+      [1, 9],
+      [5, 9],
+      [9, 9],
+    ])
 
-  expect(context.lineTo.mock.calls).toEqual([
-    [9, 1],
-    [9, 5],
-    [9, 9],
-    [1, 9],
-    [5, 9],
-    [9, 9],
-  ])
-
-  expect(context.stroke.mock.calls).toEqual([[]])
+    expect(context.stroke.mock.calls).toEqual([[]])
+  })
 })
 
 describe('render/renderPieces', () => {
-  it('renders pieces', () => {})
+  it('renders pieces', () => {
+    const canvas = <HTMLCanvasElement>{
+      width: 10,
+      height: 10,
+    }
 
-  const canvas = <HTMLCanvasElement>{
-    width: 10,
-    height: 10,
-  }
+    const context = {
+      fillRect: jest.fn(),
+    }
 
-  const context = {
-    fillRect: jest.fn(),
-  }
+    const state: State = {
+      ...DEFAULT_STATE,
+      pieces: [{ row: 0, col: 1 }],
+    }
 
-  const state: State = {
-    ...DEFAULT_STATE,
-    pieces: [{ row: 0, col: 1 }],
-  }
+    renderPieces({ canvas, context: context as any, state })
 
-  renderPieces({ canvas, context: context as any, state })
-
-  expect(context.fillRect.mock.calls).toEqual([[5, 1, 4, 4]])
+    expect(context.fillRect.mock.calls).toEqual([[5, 1, 4, 4]])
+  })
 })
 
 describe('render/renderFloor', () => {
-  it('renders floor', () => {})
+  it('renders floor', () => {
+    const canvas = <HTMLCanvasElement>{
+      width: 10,
+      height: 10,
+    }
 
-  const canvas = <HTMLCanvasElement>{
-    width: 10,
-    height: 10,
-  }
+    const context = {
+      fillRect: jest.fn(),
+    }
 
-  const context = {
-    fillRect: jest.fn(),
-  }
+    const state: State = {
+      ...DEFAULT_STATE,
+      floor: [{ row: 1, col: 0 }],
+    }
 
-  const state: State = {
-    ...DEFAULT_STATE,
-    floor: [{ row: 1, col: 0 }],
-  }
+    renderFloor({ canvas, context: context as any, state })
 
-  renderFloor({ canvas, context: context as any, state })
+    expect(context.fillRect.mock.calls).toEqual([[1, 5, 4, 4]])
+  })
+})
 
-  expect(context.fillRect.mock.calls).toEqual([[1, 5, 4, 4]])
+describe('render/renderGameOver', () => {
+  it('renders game over', () => {
+    const canvas = <HTMLCanvasElement>{
+      width: 10,
+      height: 10,
+    }
+
+    const context = {
+      fillText: jest.fn(),
+    }
+
+    const state: State = {
+      ...DEFAULT_STATE,
+      isGameOver: true,
+    }
+
+    renderGameOver({ canvas, context: context as any, state })
+
+    expect(context.fillText.mock.calls).toEqual([['Game Over', 5, 5]])
+  })
+
+  it('renders not game over', () => {
+    const canvas = <HTMLCanvasElement>{
+      width: 10,
+      height: 10,
+    }
+
+    const context = {
+      fillText: jest.fn(),
+    }
+
+    const state = DEFAULT_STATE
+    renderGameOver({ canvas, context: context as any, state })
+
+    expect(context.fillText.mock.calls).toEqual([])
+  })
 })
