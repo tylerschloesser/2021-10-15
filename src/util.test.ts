bc0a2b9e026777a27dca3befb1aff32c6b38b489
Tyler Schloesser <tschloes@amazon.com>
2021-10-16 09:00:52 -0700

    test bounding box

---
+++ src/util.test.ts
@@ -0,0 +1,27 @@
+import { Cell } from './game'
+import { getBoundingBox } from './util'
+
+describe('util/getBoundingBox', () => {
+  it('returns corrent tl and br', () => {
+    const piece: Cell[] = [
+      { row: 0, col: 0 },
+      { row: 1, col: 1 },
+    ]
+    expect(getBoundingBox(piece)).toEqual({
+      tl: { row: 0, col: 0 },
+      br: { row: 1, col: 1 },
+    })
+  })
+
+  it('returns corrent tl and br but more complicated', () => {
+    const piece: Cell[] = [
+      { row: 1, col: 1 },
+      { row: 2, col: 0 },
+      { row: 1, col: 2 },
+    ]
+    expect(getBoundingBox(piece)).toEqual({
+      tl: { row: 1, col: 0 },
+      br: { row: 2, col: 2 },
+    })
+  })
+})
